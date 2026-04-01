4c089b891fd77b1c6f1f61fec1b1eda0fa7a0a80
raulpra <raulprada@gmail.com>
2026-04-01 10:15:22 +0200

    Configuracion inicial del proyecto Spring Boot y dependencias Maven

---
+++ src/main/java/org/sti/api/StiApplication.java
@@ -0,0 +1,12 @@
+package org.sti.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StiApplication.class, args);
+    }
+}
